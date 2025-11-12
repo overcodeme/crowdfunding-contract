b37d53af204c26bce6db6bad90a6811e730fff5c
overcodeme <placmabadei1985@rambler.ru>
2025-11-12 17:18:54 +0300

    added few insignificant functions

--- FundMe.sol
+++ FundMe.sol
@@ -5,8 +5,13 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract FundMe {
     uint public MINIMUM_USD = 10;
 
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded; 
+
     function fund() public payable {
-        require(getConversationRate(msg.value) > MINIMUM_USD, "Not enough USD to send");
+        require(getConversationRate(msg.value) >= MINIMUM_USD, "Not enough USD to send");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += getConversationRate(msg.value);
     }
 
     // function withdraw() public {}
@@ -21,5 +26,15 @@ contract FundMe {
         uint256 ethPrice = getLatestPrice();
         uint256 EthAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return EthAmountInUsd;
+    }
+
+    function getLatestBTCPriceInETH() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
+
+    function convertUsdToEth(uint256 usdAmount, uint256 ethPrice) public pure returns (uint256) {
+        return usdAmount / ethPrice * 1e18;
     }
 }
